01f5c6d7421f8106846217f1d933caa2c087ea1d
thieu-le <thieu.le0330@gmail.com>
2024-03-28 16:02:05 -0500

    First commit

--- SoftwareDesignBackend/myapp/urls.py
+++ SoftwareDesignBackend/myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import login_view
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path('login/', login_view, name='login'),
 ]

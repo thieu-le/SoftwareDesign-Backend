52c25ccae9b8c8aa0738159cf30794008f122a29
thieu-le <thieu.le0330@gmail.com>
2024-03-29 13:53:47 -0500

    Login and register changed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,27 +7,12 @@ import FuelQuoteForm from './components/FuelQuoteForm';
 import FuelQuoteHistory from './components/FuelQuoteHistory';
 
 const App: React.FC = () => {
-  const [isLogin, setIsLogin] = useState(true);
-
-  const handleLoginSubmit = (username: string, password: string) => {
-    // Logic for handling login
-  };
-
-  const handleRegisterSubmit = (username: string, password: string) => {
-    // Logic for handling registration
-  };
-
-
-  const handleToggleForm = () => {
-    setIsLogin(!isLogin);
-  };
-
   return (
     <div className="App">
       <Router>
         <Routes>
-        <Route path="/" element={<Login onLoginSuccess={handleLoginSubmit} onRegister={handleToggleForm} />} />
-          <Route path="/register" element={<Register onSubmit={handleRegisterSubmit} onLogin={handleToggleForm} />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/profile" element={<ProfileForm />} />
           <Route path="/fuelquoteform" element={<FuelQuoteForm clientProfile={{ deliveryAddress: '123 Main St' }} />} />
           <Route path="/fuelquotehistory" element={<FuelQuoteHistory />} />        

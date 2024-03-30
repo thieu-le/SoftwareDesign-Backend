8e772d6b5ebac5407ea5fdac753cb0f87f1bbe27
Gabriel-Dau <gabrielqdau@yahoo.com>
2024-03-29 21:48:34 -0500

    REMOVE EXTRA FILES AFTER INTEGRATING

--- SoftwareDesignBackend/fuel quote backend/FuelQuoteURL.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('fuel-quote/', views.fuel_quote_form, name='fuel_quote_form'),
-    path('quote-history/', views.quote_history, name='quote_history'),
-]

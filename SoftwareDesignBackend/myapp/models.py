a51c75a48aec83a7cd65b61e64051538d79eaf70
anthony1130 <anthonymacias1130@gmail.com>
2024-04-09 00:40:55 -0500

    had tables created in the database for clientprofile, fuelquote, state, and usercredentials

--- SoftwareDesignBackend/myapp/models.py
+++ SoftwareDesignBackend/myapp/models.py
@@ -1,25 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+from django.utils.translation import gettext as _
+
+# Your models go here
+
+
+class UserCredentials(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    password = models.CharField(max_length=128)  # Encrypted password will be stored here
+
+class State(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    abbreviation = models.CharField(max_length=2, unique=True)
+
+    class Meta:
+        verbose_name = _("State")
+        verbose_name_plural = _("States")
+
+    def __str__(self):
+        return self.name
 
 class ClientProfile(models.Model):
-    # Define fields for the ClientProfile model
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=255, default="N/A")
     full_name = models.CharField(max_length=255, default="")
     address1 = models.CharField(max_length=255, default="")
     address2 = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=100, default="")
-    state = models.CharField(max_length=2, default="")
+    state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True)
     zipcode = models.CharField(max_length=10, default="")
-
-    # Add any additional fields as needed for the client profile
 
     def __str__(self):
         return self.full_name
 
 class FuelQuote(models.Model):
     client = models.ForeignKey(ClientProfile, on_delete=models.CASCADE)
-    gallons_requested = models.DecimalField(max_digits=10, decimal_places=2)
+    gallons_requested = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     delivery_date = models.DateField()
     suggested_price_per_gallon = models.DecimalField(max_digits=10, decimal_places=2)
     total_amount_due = models.DecimalField(max_digits=10, decimal_places=2)

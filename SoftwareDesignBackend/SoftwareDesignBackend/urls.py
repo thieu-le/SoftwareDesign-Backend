4672a36d6101aec929a73c4c4b7c7f2ba901be5b
anthony1130 <anthonymacias1130@gmail.com>
2024-04-24 22:29:08 -0500

    testing

--- SoftwareDesignBackend/SoftwareDesignBackend/urls.py
+++ SoftwareDesignBackend/SoftwareDesignBackend/urls.py
@@ -3,7 +3,7 @@ from myapp import views  # Import views from your app
 
 urlpatterns = [
     path('', include('myapp.urls')),  # Make sure this line is correctly configured
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('login/', include("myapp.urls")),
     path('profile/', views.create_client_profile, name='create_client_profile'),  # Add URL pattern for profile creation
     path('csrf/token/', views.get_csrf_token, name='get_csrf_token'),  # Add URL pattern for fetching CSRF token
